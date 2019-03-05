1597b7e94b1a332535e91a86ccbbb4fb694e2ecd
tfe-testing-1 <47905359+tfe-testing-1@users.noreply.github.com>
2019-03-05 08:14:51 -0700

    Update example.tf

--- example.tf
+++ example.tf
@@ -1,15 +1,3 @@
-provider "aws" {
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
-  region     = "${var.region}"
+variable "domain" {
+  default = "example.com"
 }
-
-resource "aws_instance" "web" {
-  ami = "ami-b374d5a5dsffsdf"
-  instance_type = "t2.micro"
-
-  provisioner "local-exec" {
-    command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
-  }
-}
-
